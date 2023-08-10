a7f89f2673bc37a6d7c3bc8873e4f1e4c60ab4bc
Marceloviniciuss <92002470+Marceloviniciuss@users.noreply.github.com>
2023-08-10 01:41:52 -0300

    Add files via upload

---
+++ curso java/TestaVariavelEFluxo/src/variaveis/PontoFlutuante.java
@@ -0,0 +1,14 @@
+package variaveis;
+
+public class PontoFlutuante {
+	public static void main(String[] args) {
+		
+		double hora = 3;
+		System.out.println("Agora são exatamente:" + hora + "horas");
+		
+		double conta = 3.4 / 2;
+		System.out.println(conta);
+	}
+	}
+
+	
---
+++ curso java/diojavabasico/README.md
@@ -0,0 +1,3 @@
+# diojavabasico
+Repositorio para armazenar todo o codigo do curso de java basico
+Alterando o conteudo de um arquivo de forma local
